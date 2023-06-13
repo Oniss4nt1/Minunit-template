4c13163d9e07ac914d99922f972293d49abbb230
Bruno Rodrigues onissanti <brunrodr@c3r3p4.42sp.org.br>
2023-06-13 19:37:23 -0300

    V1.1: test_pointer.c refatorado

--- src/ehpositivo.c
+++
@@ -1,6 +0,0 @@
-int ehpositivo(int n)
-{
-	if (n >= 0)
-		return (1);
-	return (0);
-}
--- tests/test_pointer.c
+++ tests/test_pointer.c
@@ -2,16 +2,6 @@
 #include <stdio.h>
 #include "../src/ft_printf.h"
 
-int	ft_printf(const char *str, ...);
-int	convert_specifiers(const char *format, va_list args);
-int print_char(int arg);
-int print_pointer(void *arg, int base);
-void ft_putchar(char c);
-int putnbr_ptr(unsigned long int num, int base);
-
-
-
-
 MU_TEST(test_pointer_value_is_negative)
 {
 	int valor_do_ponteiro = -42;
